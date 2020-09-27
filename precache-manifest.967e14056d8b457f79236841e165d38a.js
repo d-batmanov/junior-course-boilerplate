29ceb9badf3be597eb3ffe054482852681d9be19
d-batmanov <d-batmanov@users.noreply.github.com>
2020-09-27 20:09:04 +0000

    deploy: a085056cdac15a0469968f08df054aeb69bdaf63

---
+++ precache-manifest.967e14056d8b457f79236841e165d38a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08812156e599af464d8275e3f248d067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76836de3a2d20cda5710",
+    "url": "/static/js/2.fc5902dd.chunk.js"
+  },
+  {
+    "revision": "641f62a602193e16b86f",
+    "url": "/static/js/main.d5ac5cc4.chunk.js"
+  },
+  {
+    "revision": "a4c7d18271b1f6b3fbe9",
+    "url": "/static/js/runtime-main.abbf2941.js"
+  }
+]);
